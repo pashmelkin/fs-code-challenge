7ae6d151bd68e41575054d00b4d6e03af43d861b
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-12-10 14:46:04 +1100

    Clean up

--- dxp-dinosaur-be/routes/settings.js
+++ dxp-dinosaur-be/routes/settings.js
@@ -3,28 +3,23 @@
 const express = require('express');
 const router = express.Router();
 const config = require('../config');
-//const Settings = require('../entities/Settings');
+const Settings = require('../entities/Settings');
 
 
 router.put('/', function(req, res) {
-//    const settings = new Settings(
-//      req.body.numberOfEggs,
-//      req.body.rotation,
-//      req.body.sequence);
+    const settings = new Settings(
+      req.body.numberOfEggs,
+      req.body.rotation,
+      req.body.sequence);
 
     //  let errors = settings.Validate();
         //if errors.array = =0 then set to config
         // else return error
       console.log(req.body);    // Prints the JSON object
 
-
-    console.log( req.body.numberOfEggs + ' ' + req.body.rotation + ' ' + req.body.sequence);
-    let numberEggs = req.body.numberOfEggs;
-    let rotation = req.body.rotation;
-    let sequence = req.body.sequence;
-    config.numberEggs = numberEggs;
-    config.rotation = rotation;
-    config.sequence = sequence;
+    config.numberEggs = settings.numberEggs;
+    config.rotation = settings.rotation;
+    config.sequence = settings.sequence;
     console.log(config.numberEggs + ' ' + config.rotation + ' ' + config.sequence);
    res.send(config.numberEggs + ' ' + config.rotation + ' ' + config.sequence);
 });
--- dxp-dinosaur-be/scripts/deploy.sh
+++
@@ -1,2 +0,0 @@
-myob-auth k -e europa-preprod
-kubectl apply -f ./scripts/incident.yaml -n training
