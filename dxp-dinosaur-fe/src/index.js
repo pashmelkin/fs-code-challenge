fd11893f61a9857150ee5e00b41c1954cedc3d4d
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-12-10 14:35:13 +1100

    Add Router package

--- dxp-dinosaur-fe/src/index.js
+++ dxp-dinosaur-fe/src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import Settings from './Settings';
-
-import "./index.css";
-
 
 class App extends React.Component {
   constructor(props)
@@ -16,6 +14,7 @@ class App extends React.Component {
         <div>
           <Settings />
         </div>
+      </>
     )
   }
 }
