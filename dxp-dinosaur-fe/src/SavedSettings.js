944949075fd7b7193af911b849f7aad87a28a9b8
Pavel Shmueli <Pavel.Shmueli@myob.com>
2019-12-12 13:39:46 +1100

    Add HandleClick for Run button

--- dxp-dinosaur-fe/src/SavedSettings.js
+++ dxp-dinosaur-fe/src/SavedSettings.js
@@ -2,10 +2,37 @@ import React from "react";
 
 class SavedSettings extends React.Component {
 
+  sendRunCommnad = (callback) => {
+    fetch('http://localhost:3001/run', {
+      method: 'POST'
+    }
+    ).then(response => {
+      if (!response.ok) {
+        response.json().then(json => {
+          callback(json);
+        });
+      } else {
+        callback();
+      }
+    });
+  };
+
+  handleClick = () => {
+
+    this.sendRunCommnad(jsonBody => {
+        let result = JSON.stringify(jsonBody, null, 2);
+        console.log(result);
+        if (result === undefined)
+        {
+          this.props.onSettingsSet("SavedSettings");
+        }
+    })
+  };
+
   render() {
     return (
         <div>
-          <button type="submit"> Run</button>
+          <button type="submit" onClick={this.handleClick}> Run</button>
         </div>
     )
   }
