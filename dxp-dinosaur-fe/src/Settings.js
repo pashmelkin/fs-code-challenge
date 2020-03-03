8c52f58c6b263dcd7e48eef1ba1156436f897247
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-03-03 11:46:19 +1100

    Done

--- dxp-dinosaur-fe/src/Settings.js
+++ dxp-dinosaur-fe/src/Settings.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import SettingsEntity from './entities/SettingsEntity'
-import Rotation from './Rotation'
-import Sequence from './Sequence'
-import NumEggs from './NumEggs'
 import TextComponent from './TextComponent'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -45,7 +42,8 @@ class Settings extends React.Component {
       console.log("parameter: " + parameter);
       console.log("value: " + value);
 
-      this.setState({parameter: value});
+      this.setState({[parameter]: value});
+      console.log(this.state);
   }
 
   handleClick = () => {
@@ -71,16 +69,13 @@ class Settings extends React.Component {
     <div>
       <Container  md="auto">
         <Row>
-          <NumEggs  onNumberSet={this.handleValue} />
+          <TextComponent labelText="Please enter the number of Eggs" onSet={(e) => this.handleValue("numberOfEggs", e)}/>
         </Row>
         <Row>
-          <Rotation onRotation={this.handleValue} />
+          <TextComponent labelText="Please enter the Rotation" onSet={(e) => this.handleValue("rotation", e)}/>
         </Row>
         <Row>
-          <Sequence onSequence={this.handleValue} />
-        </Row>
-        <Row>
-          <TextComponent labelText="Please enter the number of Eggs" onNumberSet={this.handleValue}/>
+          <TextComponent labelText="Please enter the sequence" onSet={(e) => this.handleValue("sequence", e)}/>
         </Row>
         <Row>
           <Button onClick={this.handleClick} variant="primary" type="submit"
--- dxp-dinosaur-fe/src/TextComponent.js
+++ dxp-dinosaur-fe/src/TextComponent.js
@@ -10,23 +10,24 @@ class TextComponent extends React.Component {
   }
 
   handleChange = event => {
-      if(event.target.value === '')
+      const value = event.target.value.trimLeft();
+
+      if(value === '')
       {
           this.setState({ Text: '' });
           return;
       }
-      const textToInt = parseInt(event.target.value, 10);
-      if (this.allnumeric(event.target.value))
+      if (this.allnumeric(value))
       {
-        this.setState({ Text: textToInt });
-      //  this.props.onNumberSet(textToInt);
+        this.setState({ Text: value });
+        this.props.onSet(value);
       }
 
    };
 
   allnumeric = inputtxt => {
       console.log(inputtxt);
-      const numbers = /^[0-9]+$/;
+      const numbers = /^[0-9 .]+$/;
       return !!inputtxt.match(numbers);
 
     };
@@ -39,8 +40,7 @@ class TextComponent extends React.Component {
           <Form>
           <Form.Group controlId="textareas" >
             <Form.Label> {this.props.labelText}</Form.Label>
-            <Form.Control type="integer" placeholder="Enter the Value"
-            min='0'
+            <Form.Control type="string" placeholder="Enter the Value"
             value={this.state.Text}
             onChange={this.handleChange} />
           </Form.Group>
